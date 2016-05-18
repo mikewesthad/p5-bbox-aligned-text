9331b2ce2bb11920089510104c9d0b0e56182d61
Michael Hadley <michaelwesthadley@gmail.com>
2016-05-18 13:31:36 -0500

    Update examples to use new code

--- examples/02-instance-p5-rotating-words/js/main.js
+++ examples/02-instance-p5-rotating-words/js/main.js
@@ -4,20 +4,21 @@
 (function () {
     
     // Globals
-    var bboxText, p;
+    var p, bboxText, bboxLabel;
     var string = "Hey!";
-    var logoText = "String";
     var fontSize = 70;
     var fontPath = "fonts/leaguegothic-regular-webfont.ttf";
 
     // Pull out the possible alignment values, so that they can be looped
     // through inside of draw
-    var hAligns = [BboxAlignedText.ALIGN.LEFT, BboxAlignedText.ALIGN.CENTER, 
-                   BboxAlignedText.ALIGN.RIGHT];
-    var vAligns = [BboxAlignedText.BASELINE.TOP, 
-                   BboxAlignedText.BASELINE.CENTER, 
-                   BboxAlignedText.BASELINE.ALPHABETIC, 
-                   BboxAlignedText.BASELINE.BOTTOM];
+    var hAligns = [BboxAlignedText.ALIGN.BOX_LEFT,
+                   BboxAlignedText.ALIGN.BOX_CENTER,
+                   BboxAlignedText.ALIGN.BOX_RIGHT];
+    var vAligns = [BboxAlignedText.BASELINE.BOX_TOP,
+                   BboxAlignedText.BASELINE.BOX_CENTER,
+                   BboxAlignedText.BASELINE.FONT_CENTER,
+                   BboxAlignedText.BASELINE.ALPHABETIC,
+                   BboxAlignedText.BASELINE.BOX_BOTTOM];
 
     startSketch();
 
@@ -36,7 +37,14 @@
 
     function setup() {
         p.createCanvas(800, 800);    
+
+        // Create the bbox text
         bboxText = new BboxAlignedText(font, string, fontSize, p);
+
+        // Create a bboxLabel to be used to display alignment labels above text
+        bboxLabel = new BboxAlignedText(font, "label", 16, p);
+        bboxLabel.setAnchor(BboxAlignedText.ALIGN.BOX_CENTER,
+                            BboxAlignedText.BASELINE.BOX_BOTTOM);
     }
 
     function draw() {
@@ -46,21 +54,30 @@
         var rotationSpeed = p.TWO_PI / 6; // Rotations per second
         var rotation = elapsedSeconds * rotationSpeed;
 
-        for (var h = 0; h < 3; h += 1) {
-            for (var v = 0; v < 4; v += 1) {
+        for (var h = 0; h < hAligns.length; h += 1) {
+            for (var v = 0; v < vAligns.length; v += 1) {
+                // Pull out the alignment values
                 var hAlign = hAligns[h];
                 var vAlign = vAligns[v];
 
-                // Get an (x, y) coordinate that is evenly spaced 
+                // Get an (x, y) anchor coordinate, evenly spaced across screen 
                 var x = p.map(h, 0, hAligns.length - 1, 125, p.width - 125);
                 var y = p.map(v, 0, vAligns.length - 1, 100, p.height - 100);
 
+                // Draw a label saying what the anchor point is
+                p.fill(150);
+                p.noStroke();
+                bboxLabel.setText(hAlign + " - " + vAlign);
+                bboxLabel.draw(x, y - 50);
+
+                // Draw the text
                 p.fill("#00A8EA");
                 p.noStroke();
                 bboxText.setRotation(rotation);
                 bboxText.setAnchor(hAlign, vAlign);
                 bboxText.draw(x, y, true);
 
+                // Draw the anchor point
                 p.stroke(0);
                 p.fill("#FF8132");
                 p.ellipse(x, y, 8, 8);
